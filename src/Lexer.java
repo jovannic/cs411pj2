c5fc5a0921bbdea1af704a3edf4a6051e920b8d5
Jovanni Cutigni <thejacx@gmail.com>
2014-02-24 22:12:44 -0800

    Move Token out of it's parent's house

--- src/Lexer.java
+++ src/Lexer.java
@@ -8,21 +8,6 @@ import java.util.Scanner;
  * @author Jovanni Cutigni
  */
 public class Lexer {
-    public enum Token {
-        _bool, _break, _class, _double,
-        _else, _extends, _for, _if,
-        _implements, _int, _interface, _newarray,
-        _println, _readln, _return, _string,
-        _void, _while, _plus, _minus,
-        _multiplication, _division, _less, _lessequal,
-        _greater, _greaterequal, _equal, _notequal,
-        _and, _or, _not, _assignop,
-        _semicolon, _comma, _period, _leftparen,
-        _rightparen, _leftbracket, _rightbracket, _leftbrace,
-        _rightbrace, _boolconstant, _intconstant, _doubleconstant,
-        _stringconstant, _id
-    }
-
     SymbolMap<Token> keywords = new HashSymbolMap<Token>();
     TrieMap<Token> operators = new TrieMap<Token>();
 
---
+++ src/Token.java
@@ -0,0 +1,17 @@
+/**
+* @author Jovanni Cutigni
+*/
+public enum Token {
+    _bool, _break, _class, _double,
+    _else, _extends, _for, _if,
+    _implements, _int, _interface, _newarray,
+    _println, _readln, _return, _string,
+    _void, _while, _plus, _minus,
+    _multiplication, _division, _less, _lessequal,
+    _greater, _greaterequal, _equal, _notequal,
+    _and, _or, _not, _assignop,
+    _semicolon, _comma, _period, _leftparen,
+    _rightparen, _leftbracket, _rightbracket, _leftbrace,
+    _rightbrace, _boolconstant, _intconstant, _doubleconstant,
+    _stringconstant, _id
+}
