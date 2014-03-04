440e8800c6eecf5f63c712ad4958e53bc57e4ac2
Jovanni Cutigni <thejacx@gmail.com>
2014-03-03 16:50:59 -0800

    Add Grammar parser. Could be better. Works.
    Didn't see Mac's before I finished this

---
+++ src/cs411/project/lexer/Token.java
@@ -0,0 +1,29 @@
+package cs411.project.lexer;
+
+/**
+ * @author Jovanni Cutigni
+ */
+public enum Token {
+    _bool("bool"), _break("break"), _class("class"), _double("double"),
+    _else("else"), _extends("extends"), _for("for"), _if("if"),
+    _implements("implements"), _int("int"), _interface("interface"), _newarray("newarray"),
+    _println("println"), _readln("readln"), _return("return"), _string("string"),
+    _void("void"), _while("while"), _plus("+"), _minus("-"),
+    _multiplication("*"), _division("/"), _less(">"), _lessequal(">="),
+    _greater(">"), _greaterequal(">="), _equal("=="), _notequal("!="),
+    _and("&&"), _or("||"), _not("!"), _assignop("="),
+    _semicolon(";"), _comma(","), _period("."), _leftparen("("),
+    _rightparen(">"), _leftbracket("["), _rightbracket("]"), _leftbrace("{"),
+    _rightbrace("}"), _boolconstant(null), _intconstant(null), _doubleconstant(null),
+    _stringconstant(null), _id(null);
+
+    private final String value;
+
+    private Token(String value) {
+        this.value = value;
+    }
+
+    public String stringValue() {
+        return value;
+    }
+}
--- src/cs411/project/pkg2/parser/CS411Project2Parser.java
+++ src/cs411/project/pkg2/parser/CS411Project2Parser.java
@@ -4,18 +4,23 @@
  */
 package cs411.project.pkg2.parser;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
- *
+ * CS 411 Project 2 Parser
  * @author Michael
+ * @author Jovanni Cutigni
  */
 public class CS411Project2Parser {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        
+        Grammar g = Grammar.load(new File("grammar.txt"));
+
         //is this even working
     }
 }
