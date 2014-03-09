4cb5f0d2e851cdf020c4edad2538cf7408c2409f
thedancinzerg <thedancinzerg@gmail.com>
2014-03-08 18:51:20 -0800

    Forgot to add the tests.java file to the last commit

---
+++ src/cs411/project/pkg2/parser/Tests.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs411.project.pkg2.parser;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Michael
+ */
+public class Tests {
+    public static void runAllTests() {
+        tableTest();
+    }
+    
+    private static void tableTest() {
+        LinkedList<LinkedList<Integer>> l = new LinkedList();
+        LinkedList<Integer> li = new LinkedList();
+        li.add(new Integer(1));
+        li.add(new Integer(0));
+        li.add(new Integer(5));
+        li.add(new Integer(2));
+        l.add(li);
+        li = new LinkedList();
+        li.add(new Integer(2));
+        li.add(new Integer(0));
+        li.add(new Integer(6));
+        l.add(li);
+        Table t = new Table(l,4);
+        t.makeAi();
+        printList(t.getLists());
+    }
+    
+    private static void printList(LinkedList<LinkedList<LinkedList<Integer>>> l) {
+        for(int i = 0; i < l.size(); i++) {
+            System.out.println("Table number: " + i);
+            for (int j = 0; j < l.get(i).size(); j++) {
+                System.out.println();
+                for(int k = 0; k < l.get(i).get(j).size(); k++) {
+                    System.out.print(l.get(i).get(j).get(k) + " ");
+                    
+                }
+            }
+        }
+    }
+}
