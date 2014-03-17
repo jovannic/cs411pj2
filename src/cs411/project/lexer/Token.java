03ebc0f8ae95dbc3e5a29542dc5b060c28ea539b
Jovanni Cutigni <thejacx@gmail.com>
2014-03-16 19:26:25 -0700

    Fix Token._less and buddy

--- src/cs411/project/lexer/Token.java
+++ src/cs411/project/lexer/Token.java
@@ -9,7 +9,7 @@ public enum Token {
     _implements("implements"), _int("int"), _interface("interface"), _newarray("newarray"),
     _println("println"), _readln("readln"), _return("return"), _string("string"),
     _void("void"), _while("while"), _plus("+"), _minus("-"),
-    _multiplication("*"), _division("/"), _less(">"), _lessequal(">="),
+    _multiplication("*"), _division("/"), _less("<"), _lessequal("<="),
     _greater(">"), _greaterequal(">="), _equal("=="), _notequal("!="),
     _and("&&"), _or("||"), _not("!"), _assignop("="),
     _semicolon(";"), _comma(","), _period("."), _leftparen("("),
