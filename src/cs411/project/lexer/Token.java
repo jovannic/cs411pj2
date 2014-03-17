44fe17e8f2ac39889da6d09223ce91d8282b1902
Jovanni Cutigni <thejacx@gmail.com>
2014-03-17 16:23:17 -0700

    Carry over _rightparen(">"), etc. fix

--- src/cs411/project/lexer/Token.java
+++ src/cs411/project/lexer/Token.java
@@ -9,11 +9,11 @@ public enum Token {
     _implements("implements"), _int("int"), _interface("interface"), _newarray("newarray"),
     _println("println"), _readln("readln"), _return("return"), _string("string"),
     _void("void"), _while("while"), _plus("+"), _minus("-"),
-    _multiplication("*"), _division("/"), _less(">"), _lessequal(">="),
+    _multiplication("*"), _division("/"), _less("<"), _lessequal("<="),
     _greater(">"), _greaterequal(">="), _equal("=="), _notequal("!="),
     _and("&&"), _or("||"), _not("!"), _assignop("="),
     _semicolon(";"), _comma(","), _period("."), _leftparen("("),
-    _rightparen(">"), _leftbracket("["), _rightbracket("]"), _leftbrace("{"),
+    _rightparen(")"), _leftbracket("["), _rightbracket("]"), _leftbrace("{"),
     _rightbrace("}"), _boolconstant(null), _intconstant(null), _doubleconstant(null),
     _stringconstant(null), _id(null);
 
